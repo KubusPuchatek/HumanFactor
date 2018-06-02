94fec392b77aedfda68cb65899483abdd7c9322c
Łukasz Doszczeczko <ldos@dgs.com>
2018-06-02 10:14:32 +0200

    dummy Company view added

---
+++ UI/Company/CompanyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HumanFactor;
+using UI.Helpers;
+
+namespace UI.Company
+{
+    internal class CompanyViewModel : ObservableObject
+    {
+        private KrsModel _krsCompany;
+
+        internal KrsModel KrsCompany
+        {
+            get => _krsCompany;
+            set
+            {
+                _krsCompany = value;
+                OnPropertyChanged("KrsComapny");
+            }
+        }
+
+        protected CompanyViewModel(KrsModel company)
+        {
+            KrsCompany = company;
+        }
+    }
+}
